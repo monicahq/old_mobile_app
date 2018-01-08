bd45b84163ac52ee1161307b4fb3e6e729949e19
Théo Mathieu <theo.mathieu1@gmail.com>
2018-01-07 20:29:26 -0500

    Added LaunchScreen
    Fixed tests

---
+++ src/components/Button/Button.android.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { TouchableNativeFeedback, View, Text } from 'react-native';
+import { styles } from './Button.styles';
+
+export class Button extends Component {
+  static propTypes = {
+    onPress: PropTypes.func,
+    text: PropTypes.string,
+    children: PropTypes.string,
+  }
+
+  getText () {
+    const buttonText = this.props.text.toUpperCase() || this.props.children || '';
+    return buttonText;
+  }
+
+  render () {
+    return (
+      <TouchableNativeFeedback onPress={this.props.onPress}
+      background={TouchableNativeFeedback.SelectableBackground()}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>{this.getText()}</Text>
+        </View>
+      </TouchableNativeFeedback>
+    );
+  }
+}
---
+++ src/components/Button/Button.styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  button: {
+    height: 45,
+    borderRadius: 5,
+    marginVertical: 10,
+    backgroundColor: '#2BBB48',
+    justifyContent: 'center',
+    width: '100%'
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    // fontSize: Fonts.size.medium,
+    // marginVertical: Metrics.baseMargin
+  }
+});
