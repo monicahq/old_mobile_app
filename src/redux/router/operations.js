ddafa780f16c3c2f7ddb46a90e88c9c554db2cae
Théo Mathieu <theo.mathieu1@gmail.com>
2018-01-27 11:24:09 -0500

    Reset bar style when a user logs out

--- src/redux/router/operations.js
+++ src/redux/router/operations.js
@@ -1,4 +1,5 @@
 import * as actions from './actions';
+import {StatusBar} from 'react-native';
 
 export function navigate(routeName, params) {
   return actions.navigate(routeName, params);
@@ -13,5 +14,6 @@ export function setState(state) {
 }
 
 export function goToLaunchScreen() {
+  StatusBar.setBarStyle('default');
   return actions.goToLaunchScreen();
 }
