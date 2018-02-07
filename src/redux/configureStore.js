b15b7fa8c001a753538e88ced20664548e3e98f9
Théo Mathieu <theo.mathieu1@gmail.com>
2018-02-06 23:38:39 -0500

    Update react navigation to 1.0.0

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,27 @@
 import {createStore, compose, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {devToolsEnhancer} from 'redux-devtools-extension';
+import {createReactNavigationReduxMiddleware} from 'react-navigation-redux-helpers';
 
 import rootReducer from './rootReducer';
+
+const rnNavigationMiddleware = createReactNavigationReduxMiddleware(
+  'root',
+  state => state.nav,
+);
 
 export default function configureStore() {
   return createStore(
     rootReducer,
     __DEV__
-      ? compose(applyMiddleware(thunk), devToolsEnhancer())
-      : applyMiddleware(thunk),
+      ? compose(
+          applyMiddleware(thunk),
+          applyMiddleware(rnNavigationMiddleware),
+          devToolsEnhancer(),
+        )
+      : compose(
+          applyMiddleware(thunk),
+          applyMiddleware(rnNavigationMiddleware),
+        ),
   );
 }
