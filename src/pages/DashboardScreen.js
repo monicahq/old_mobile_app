6f615401d955bf69eee5239be193b730ab55353a
Théo Mathieu <theo.mathieu1@gmail.com>
2018-02-07 20:27:46 -0500

    Added Under Construction pages

--- src/pages/DashboardScreen.js
+++ src/pages/DashboardScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {View} from 'react-native';
 
-import {Navbar} from 'components';
+import {Navbar, UnderConstruction} from 'components';
 import {commonStyles} from 'theme';
 
 export const DashboardScreen = () => (
   <View style={commonStyles.flex}>
     <Navbar title="Dashboard" />
+    <UnderConstruction />
   </View>
 );
