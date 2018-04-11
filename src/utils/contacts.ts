6b9f2770ee125a7c66b8a94a7c3d7e8ffb80063a
Théo Mathieu <theo.mathieu1@gmail.com>
2018-04-10 20:03:58 -0400

    fix(android): contact page toolbar

--- src/utils/contacts.ts
+++ src/utils/contacts.ts
@@ -1,25 +1,33 @@
 import {IContact} from '@models';
 import moment from 'moment';
 
-export const getAvatarUrl = (contact: IContact) => {
+export const getName = (contact: IContact): string => {
+  let name = contact.first_name;
+  if (contact.last_name) {
+    name += ' ' + contact.last_name;
+  }
+  return name;
+};
+
+export const getAvatarUrl = (contact: IContact): string => {
   const avatar = contact.information.avatar;
   if (avatar && avatar.url) {
     return avatar.url;
   }
 };
 
-export const getAvatarColor = (contact: IContact) => {
+export const getAvatarColor = (contact: IContact): string => {
   const avatar = contact.information.avatar;
   if (avatar && avatar.default_avatar_color) {
     return avatar.default_avatar_color;
   }
 };
 
-export const getLastUpdatedDate = (contact: IContact) => {
+export const getLastUpdatedDate = (contact: IContact): string => {
   return moment(contact.updated_at).format('L');
 };
 
-export const getAge = (contact: IContact) => {
+export const getAge = (contact: IContact): number => {
   const birthDate =
     contact &&
     contact.information &&
@@ -32,7 +40,7 @@ export const getAge = (contact: IContact) => {
   return moment().diff(birthDate.date, 'years');
 };
 
-export const getInitials = (contact: IContact) => {
+export const getInitials = (contact: IContact): string => {
   return (
     (contact.first_name ? contact.first_name.charAt(0) : '') +
     (contact.last_name ? contact.last_name.charAt(0) : '')
