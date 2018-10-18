3b0889afcf942c39017fcec13649d91710fb50db
Michael Lynch <git@mtlynch.io>
2018-10-17 23:20:47 -0500

    Fixed grammar error on launch page text
    The phrase 'friend and family' is awkward in English because 'friend' refers to a single person whereas 'family' refers to a group of people.
    
    Changing to 'friends and family' for a more natural English phrasing.

--- src/i18n/locales/en/auth.ts
+++ src/i18n/locales/en/auth.ts
@@ -1,5 +1,5 @@
 export const auth = {
-  launch1: `Keep track of every interaction with your friend and family!`,
+  launch1: `Keep track of every interaction with your friends and family!`,
   launch2: `Remember when you last called, what you talked about, the name of their kids or what they prefer to eat!`,
   signinTitle: `Sign in with your account`,
   yourEmail: 'Your email',
